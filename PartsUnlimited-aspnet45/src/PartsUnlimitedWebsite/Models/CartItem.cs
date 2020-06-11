f63d9f6072876870aa724b8bf04dc83a922243b3
Mihir Govani <mihirgovani.dev@gmail.com>
2020-06-10 19:55:01 -0500

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -17,6 +17,6 @@ namespace PartsUnlimited.Models
         [DataType(DataType.DateTime)]
         public DateTime DateCreated { get; set; }
 
-        public virtual Product Product { get; set; }
+        public virtual Product Product { get; set; } //My Second Change
     }
 }
